59e507b7ef5a6661c36bcdafeeecbb72825e0b08
Cao Nguyen Duc Huy <duchuy231132@gmail.com>
2024-11-22 17:11:37 +0700

    hihihihi
    hihiihhi

--- assets/js/card.js
+++ assets/js/card.js
@@ -4,81 +4,565 @@ let products = [
     {
         id: 1,
         cardType: 'Monster',
-        name: 'Bahamut Shark',
+        name: 'Blue-eyes Alternative White Dragon',
         price: 10000,
-        image: 'assets/card/Bahamut_Shark.jpg'
+        sl:13,
+        image: 'assets/card/BE_Alternative.jpg'
     },
     {
         id: 2,
         cardType: 'Monster',
-        name: 'Blue-eyes Alternative White Dragon',
+        name: 'Blue-eyes Chaos Dragon',
         price: 10000,
-        image: 'assets/card/BE_Alternative.jpg'
+        sl:3,
+        image: 'assets/card/BE_Chaos.jpg'
     },
     {
         id: 3,
         cardType: 'Monster',
-        name: 'Blue-eyes Chaos Dragon',
+        name: 'Blue-eyes Chaos MAX Dragon',
         price: 10000,
-        image: 'assets/card/BE_Chaos.jpg'
+        sl:2,
+        image: 'assets/card/BE_Chaos_MAX.jpg'
     },
     {
         id: 4,
         cardType: 'Monster',
-        name: 'Blue-eyes Chaos MAX Dragon',
+        name: 'Blue Eyes White Dragon',
         price: 10000,
-        image: 'assets/card/BE_Chaos_MAX.jpg'
+        sl:4,
+        image: './assets/card/BE_White_Dragon.jpg'
     },
     {
         id: 5,
         cardType: 'Monster',
-        name: 'Blue-eyes Jet Dragon',
-        price: 10000,
-        image: 'assets/card/BE_Spirit.jpg'
+        name: 'Destiny HERO - Plasma',
+        price: 20000,
+        sl:4,
+        image: './assets/card/D_Hero_Plasma.jpg'
     },
     {
         id: 6,
-        cardType: 'Spell',
-        name: 'Super Polymerization',
-        price: 500000,
-        image: './assets/card/Super_Poly.jpg'
+        cardType: 'Monster',
+        name: 'Dark Magician',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Dark_magician.jpg'
     },
     {
         id: 7,
-        cardType: 'Trap',
-        name: 'Mirror Force',
-        price: 30000,
-        image: './assets/card/Mirror_Force.jpg'
+        cardType: 'Monster',
+        name: 'Dark Magician Girl',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Dark_magician_girl.jpg'
     },
     {
         id: 8,
-        cardType: 'Trap',
-        name: 'Mirror Force',
-        price: 30000,
-        image: './assets/card/Mirror_Force.jpg'
+        cardType: 'Spell',
+        name: 'Dark Ruler No More',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Dark_Ruler_No_More.jpg'
     },
     {
         id: 9,
-        cardType: 'Trap',
-        name: 'Mirror Force',
-        price: 30000,
-        image: './assets/card/Mirror_Force.jpg'
+        cardType: 'Monster',
+        name: 'Elemental HERO Absolute Zero',
+        price: 10000,
+        sl:4,
+        image: './assets/card/E_Hero_Absolute_Zero.jpg'
     },
     {
         id: 10,
-        cardType: 'Trap',
-        name: 'Mirror Force',
-        price: 30000,
-        image: './assets/card/Mirror_Force.jpg'
+        cardType: 'Monster',
+        name: 'Elemental HERO Blazeman',
+        price: 10000,
+        sl:4,
+        image: './assets/card/E_Hero_Blazeman.jpg'
     },
     {
         id: 11,
+        cardType: 'Monster',
+        name: 'Elemental HERO Escuridao',
+        price: 10000,
+        sl:4,
+        image: './assets/card/E_Hero_Escuridao.jpg'
+    },
+    {
+        id: 12,
+        cardType: 'Monster',
+        name: 'Elemental HERO Gaia',
+        price: 10000,
+        sl:4,
+        image: './assets/card/E_Hero_Gaia.jpg'
+    },
+    {
+        id: 13,
+        cardType: 'Monster',
+        name: 'Elemental HERO Great Tornado',
+        price: 10000,
+        sl:4,
+        image: './assets/card/E_Hero_Great_Tornado.jpg'
+    },
+    {
+        id: 14,
+        cardType: 'Monster',
+        name: 'Elemental HERO Liquid Solider',
+        price: 10000,
+        sl:4,
+        image: './assets/card/E_Hero_Liquid_Solider.jpg'
+    },
+    {
+        id: 15,
+        cardType: 'Monster',
+        name: 'Elemental HERO Neos',
+        price: 10000,
+        sl:4,
+        image: './assets/card/E_Hero_Neos.jpg'
+    },
+    {
+        id: 16,
+        cardType: 'Monster',
+        name: 'Elemental HERO Prisma',
+        price: 10000,
+        sl:4,
+        image: './assets/card/E_Hero_Prisma.jpg'
+    },
+    {
+        id: 17,
+        cardType: 'Monster',
+        name: 'Elemental HERO Shadow Mist',
+        price: 10000,
+        sl:4,
+        image: './assets/card/E_Hero_Shadow_Mist.jpg'
+    },
+    {
+        id: 18,
+        cardType: 'Spell',
+        name: 'Effect Veiler',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Effect_Veiler.jpg'
+    },
+    {
+        id: 19,
+        cardType: 'Monster',
+        name: 'Exodia',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Exodia.jpg'
+    },
+    {
+        id: 20,
+        cardType: 'Monster',
+        name: 'Left Arm Of The Forbidden One ',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Exodia_Left_Arm.jpg'
+    },
+    {
+        id: 21,
+        cardType: 'Monster',
+        name: 'Left Leg Of The Forbidden One',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Exodia_Left_Leg.jpg'
+    },
+    {
+        id: 22,
+        cardType: 'Monster',
+        name: 'Right Arm Of The Forbidden One',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Exodia_Right_Arm.jpg'
+    },
+    {
+        id: 23,
+        cardType: 'Monster',
+        name: 'Right Leg Of The Forbidden One',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Exodia_Right_Leg.jpg'
+    },
+    {
+        id: 24,
+        cardType: 'Monster',
+        name: 'Galaxy-eyes Afterglow Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/GE_Afterglow.jpg'
+    },
+    {
+        id: 25,
+        cardType: 'Monster',
+        name: 'Galaxy-eyes Cipher Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/GE_Cipher.jpg'
+    },
+    {
+        id: 26,
+        cardType: 'Monster',
+        name: 'Galaxy-eyes Cipher Blade Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/GE_CipherBlade.jpg'
+    },
+    {
+        id: 27,
+        cardType: 'Monster',
+        name: 'Galaxy-eyes CipherX Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/GE_CipherX.jpg'
+    },
+    {
+        id: 28,
+        cardType: 'Monster',
+        name: 'Galaxy-eyes Full Armor Photon Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/GE_Full_Armor.jpg'
+    },
+    {
+        id: 29,
+        cardType: 'Monster',
+        name: 'Galaxy-eyes Photon Dragon',
+        price: 99999999,
+        sl:4,
+        image: './assets/card/GE_Photon.jpg'
+    },
+    {
+        id: 30,
         cardType: 'Trap',
-        name: 'Mirror Force',
-        price: 30000,
-        image: './assets/card/Mirror_Force.jpg'
+        name: 'Infinite Impermanence',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Infinite_Impermanence.jpg'
+    },
+    {
+        id: 31,
+        cardType: 'Monster',
+        name: 'Junkuriboh',
+        price: 10000,
+        sl:4,
+        image: './assets/card/junkuriboh.jpg'
+    },
+    {
+        id: 32,
+        cardType: 'Monster',
+        name: 'Kuriboh',
+        price: 10000000000,
+        sl:4,
+        image: './assets/card/kuriboh.jpg'
+    },
+    {
+        id: 33,
+        cardType: 'Monster',
+        name: 'Linkuriboh',
+        price: 10000,
+        sl:4,
+        image: './assets/card/linkuriboh.jpg'
+    },
+    {
+        id: 34,
+        cardType: 'Spell',
+        name: 'Monster Reborn',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Monster_Reborn.jpg'
+    },
+    {
+        id: 35,
+        cardType: 'Spell',
+        name: 'Mystical Space Typhoon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/MST.jpg'
+    },
+    {
+        id: 36,
+        cardType: 'Monster',
+        name: 'Neo Galaxy-Eyes Cipher Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Neo_GE_Cipher.jpg'
+    },
+    {
+        id: 37,
+        cardType: 'Monster',
+        name: 'Neo Galaxy-Eyes Photon Dragon',
+        price: 4444444,
+        sl:4,
+        image: './assets/card/Neo_GE_Photon.jpg'
+    },
+    {
+        id: 38,
+        cardType: 'Monster',
+        name: 'Nibiru, The Primal Being',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Nibiru.jpg'
+    },
+    {
+        id: 39,
+        cardType: 'Monster',
+        name: 'Number 62: Galaxy-Eyes Prime Photon Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_62.jpg'
+    },
+    {
+        id: 40,
+        cardType: 'Monster',
+        name: 'Number 73: Abyss Splash',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_73.jpg'
+    },
+    {
+        id: 41,
+        cardType: 'Monster',
+        name: 'Number 100: Numeron Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_100.jpg'
+    },
+    {
+        id: 42,
+        cardType: 'Monster',
+        name: 'Number 101: Silent Honor ARK',
+        price: 1010101010101,
+        sl:1,
+        image: './assets/card/Number_101.jpg'
+    },
+    {
+        id: 43,
+        cardType: 'Monster',
+        name: 'Number 102: Star Seraph Sentry',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_102.jpg'
+    },
+    {
+        id: 44,
+        cardType: 'Monster',
+        name: 'Number 103: Ragnazero',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_103.jpg'
+    },
+    {
+        id: 45,
+        cardType: 'Monster',
+        name: 'Number 104: Masquerade',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_104.jpg'
+    },
+    {
+        id: 46,
+        cardType: 'Monster',
+        name: "Number 105: Battlin' Boxer Star Cestus",
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_105.jpg'
+    },
+    {
+        id: 47,
+        cardType: 'Monster',
+        name: 'Number 106: Giant Hand',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_106.jpg'
+    },
+    {
+        id: 48,
+        cardType: 'Monster',
+        name: 'Number 107: Galaxy-Eyes Tachyon Dragon',
+        price: 107701,
+        sl:107,
+        image: './assets/card/Number_107.jpg'
+    },
+    {
+        id: 49,
+        cardType: 'Monster',
+        name: 'Number C73: Abyss Supra Splash',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_C73.jpg'
+    },
+    {
+        id: 50,
+        cardType: 'Monster',
+        name: 'Number C101: Silent Honor DARK',
+        price: 101101101101,
+        sl:111,
+        image: './assets/card/Number_C101.jpg'
+    },
+    {
+        id: 51,
+        cardType: 'Monster',
+        name: 'Number C102: Archfiend Seraph',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_C102.jpg'
+    },
+    {
+        id: 52,
+        cardType: 'Monster',
+        name: 'Number C103: Ragnafinity',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_C103.jpg'
+    },
+    {
+        id: 53,
+        cardType: 'Monster',
+        name: 'Number C104: Umbral Horror Masquerade',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Number_C104.jpg'
+    },
+    {
+        id: 54,
+        cardType: 'Monster',
+        name: "Number C105: Battlin' Boxer Comet Cestus",
+        price: 10000,
+        sl:105,
+        image: './assets/card/Number_C105.jpg'
+    },
+    {
+        id: 55,
+        cardType: 'Monster',
+        name: 'Number C106: Giant Red Hand',
+        price: 10000,
+        sl:106,
+        image: './assets/card/Number_C106.jpg'
+    },
+    {
+        id: 56,
+        cardType: 'Monster',
+        name: 'Number C107: Neo Galaxy-Eyes Tachyon Dragon',
+        price: 107000,
+        sl:107,
+        image: './assets/card/Number_C107.jpg'
+    },
+    {
+        id: 57,
+        cardType: 'Monster',
+        name: 'Obelisk The Tormentor',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Obelisk.jpg'
+    },
+    {
+        id: 58,
+        cardType: 'Spell',
+        name: 'Polymerization',
+        price: 696969,
+        sl:4,
+        image: './assets/card/Polymerization.jpg'
+    },
+    {
+        id: 59,
+        cardType: 'Spell',
+        name: 'Pot Of Greed',
+        price: 22222,
+        sl:4,
+        image: './assets/card/pot_of_greed.jpg'
+    },
+    {
+        id: 60,
+        cardType: 'Monster',
+        name: 'The Winged Dragon of Ra',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Ra.jpg'
+    },
+    {
+        id: 61,
+        cardType: 'Monster',
+        name: 'Red-Eyes Black Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/RE_Black.jpg'
+    },
+    {
+        id: 62,
+        cardType: 'Monster',
+        name: 'Red Dragon Archfiend',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Red_Demon_Drag.jpg'
+    },
+    {
+        id: 63,
+        cardType: 'Monster',
+        name: 'Relinkuriboh',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Relinkuriboh.jpg'
+    },
+    {
+        id: 64,
+        cardType: 'Spell',
+        name: 'Rank-Up-Magic - The Seventh One',
+        price: 777777,
+        sl:4,
+        image: './assets/card/RUM_7th.jpg'
+    },
+    {
+        id: 65,
+        cardType: 'Monster',
+        name: 'Slifer the Sky Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Slifer.jpg'
+    },
+    {
+        id: 66,
+        cardType: 'Monster',
+        name: 'Sphere Kuriboh',
+        price: 6666666,
+        sl:4,
+        image: './assets/card/sphere_kuriboh.jpg'
+    },
+    {
+        id: 67,
+        cardType: 'Monster',
+        name: 'Stardust Dragon',
+        price: 10000,
+        sl:4,
+        image: './assets/card/Stardust_Drag.jpg'
+    },
+    {
+        id: 68,
+        cardType: 'Spell',
+        name: 'Super Polymerization',
+        price: 696969,
+        sl:4,
+        image: './assets/card/Super_Poly.jpg'
+    },
+    {
+        id: 69,
+        cardType: 'Monster',
+        name: 'The Flute of Summoning Kuriboh',
+        price: 369369,
+        sl:4,
+        image: './assets/card/the_flute_of_summoning_kuriboh.jpg'
+    },
+    {
+        id: 70,
+        cardType: 'Monster',
+        name: 'Winged Kuriboh',
+        price: 999999999999,
+        sl:0,
+        image: './assets/card/winged_kuriboh.jpg'
     }
 ];
+
 let listCards = [];
 function initApp(){
     list.innerHTML = '';
@@ -90,6 +574,7 @@ function initApp(){
             <img src="${value.image}">
             <div class="name">Tên card: ${value.name}</div>
             <div class="price">Giá: ${value.price.toLocaleString()}</div>
+            <div class="sl">Số lượng còn: ${value.sl.toLocaleString()}</div>
             <button class="button" onclick="addToCart(${key})">Thêm vào giỏ hàng</button>`;
         list.appendChild(newDiv);
     })
